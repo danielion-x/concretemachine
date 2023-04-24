1660b181953c7e538c050cbeafe14d426ee36266
Alexander Khong <aokhong@dons.usfca.edu>
2023-04-23 22:56:43 -0700

    Deleted the Files at the bottom

--- concrete_analysis.py
+++ concrete_analysis.py
@@ -9,55 +9,69 @@ import PySimpleGUI as sg
 pd.options.mode.chained_assignment = None  # default='warn'
 
 class test_specimen:
-    def __init__(self, file_name, specimen_name, fineaggpcnt, coarseaggpcnt, waterpcnt, cementpcnt, blast_fer_slg, flyash, superplast, age):
-        self.file_name = file_name
-        self.specimen_name = specimen_name
-        self.radius = 2
-        self.ultimate_strength = 0
-        self.youngs_modulus = 0
-        self.data_table = 0
-        self.fine_aggregate = fineaggpcnt * 0.453592
-        self.coarse_aggregate_percent = coarseaggpcnt * 0.453592
-        self.water_percent = waterpcnt * 0.453592
-        self.cement_percent = cementpcnt * 0.453592
-        self.blast_furnace_slag = blast_fer_slg * 0.453592
-        self.fly_ash = flyash * 0.453592
-        self.super_plastisizer = superplast * 0.453592
-        self.age = age
-        self.predictive_data_table = 0
+    class test_specimen:
+        def __init__(self, file_name, specimen_name, radius, fineaggpcnt, coarseaggpcnt, waterpcnt, cementpcnt,
+                     blast_fer_slg, flyash, superplast, age, kips_column, inch_column):
+            self.file_name = file_name
+            self.specimen_name = specimen_name
+            self.radius = radius
+            self.ultimate_strength = 0
+            self.youngs_modulus = 0
+            self.data_table = 0
+            self.fine_aggregate = fineaggpcnt * 0.453592
+            self.coarse_aggregate_percent = coarseaggpcnt * 0.453592
+            self.water_percent = waterpcnt * 0.453592
+            self.cement_percent = cementpcnt * 0.453592
+            self.blast_furnace_slag = blast_fer_slg * 0.453592
+            self.fly_ash = flyash * 0.453592
+            self.super_plastisizer = superplast * 0.453592
+            self.age = age
+            self.predictive_data_table = 0
+            self.kips_column = kips_column
+            self.inch_column = inch_column
 
-    def concreteAnalysis(self):
-        ''' Takes an Excel file of inches vs kips and produces a graphical representation including
-        a scatter plot, a rolling average, and a linear regression line. Also produces key metrics
-        such as Young's Modulus and Ultimate Strength '''
-        area = (self.radius ** 2) * np.pi
-        concrete_df = pd.read_excel(self.file_name) #opens CSV file
-        concrete_dff = concrete_df[(concrete_df['kips'] >= 0.1) & (concrete_df['inch'] >= 0)] #new df with all > 0 kips and inch values
-        row_max = concrete_dff['kips'].idxmax()
+        def concreteAnalysis(self):
+            ''' Takes an Excel file of inches vs kips and produces a graphical representation including
+            a scatter plot, a rolling average, and a linear regression line. Also produces key metrics
+            such as Young's Modulus and Ultimate Strength '''
+            area = (self.radius ** 2) * np.pi
+            concrete_df = pd.read_excel(self.file_name)  # opens CSV file
+            concrete_df.rename(columns={concrete_df.columns[self.inch_column]: 'inch'}, inplace=True)
+            concrete_df.rename(columns={concrete_df.columns[self.kips_column]: 'kips'}, inplace=True)
+            concrete_dft = concrete_df[['kips', 'inch']]
+            concrete_dftt = concrete_dft.drop([0, 1])
+            concrete_dftt['kips'] = pd.to_numeric(concrete_dftt['kips'])
+            concrete_dftt['inch'] = pd.to_numeric(concrete_dftt['inch'])
 
-        concrete_dff['corrected disp'] = concrete_dff['inch'] - concrete_dff.iloc[0]['inch']
-        concrete_dff['strain'] = (concrete_dff['corrected disp'] / self.radius).truncate(after=row_max)
-        concrete_dff['stress'] = ((concrete_dff['kips'] * 1000) / area).truncate(after=row_max)
-        print('Max is %f' %concrete_dff['stress'].max())
-        self.ultimate_strength = concrete_dff['stress'].max()
-        concrete_dff['rolling'] = concrete_dff['stress'].rolling(50).mean()
-        reg_line = linregress(concrete_dff['strain'].head(5000), concrete_dff['stress'].head(5000))
-        self.youngs_modulus = reg_line.slope
-        print(self.specimen_name + " Ultimate Strength: %f. Young's Modulus: %f" % (self.ultimate_strength, self.youngs_modulus))
-        self.data_table = concrete_dff
+            concrete_dff = concrete_dftt[
+                (concrete_dftt['kips'] >= 1) & (concrete_dftt['inch'] >= 0)]  # new df with all > 0 kips and inch values
+            row_max = concrete_dff['kips'].idxmax()
 
-        fig = plt.figure()
-        plt.plot(concrete_dff['strain'], concrete_dff['stress'], label='Structural Analysis of %s' % self.specimen_name)
-        plt.plot(concrete_dff['strain'], reg_line.intercept + self.youngs_modulus * concrete_dff['strain'],
-        label='Regression Line = %fx + %f with R^2 %f' % (self.youngs_modulus, reg_line.intercept, reg_line.rvalue))
-        plt.plot(concrete_dff['strain'], concrete_dff['rolling'], label='Rolling Average')
-        plt.title('Stress vs. Strain of %s with Ultimate Strength %f' % (self.specimen_name, self.ultimate_strength))
-        plt.xlabel('Strain (in/in)')
-        plt.ylabel('Stress (psi)')
-        plt.ylim(0, self.ultimate_strength)
-        plt.legend()
-        plt.show()
-        plt.savefig('%s plot.png' % self.specimen_name, dpi=500)
+            concrete_dff['corrected disp'] = concrete_dff['inch'] - concrete_dff.iloc[0]['inch']
+            concrete_dff['strain'] = (concrete_dff['corrected disp'] / self.radius).truncate(after=row_max)
+            concrete_dff['stress'] = ((concrete_dff['kips'] * 1000) / area).truncate(after=row_max)
+            self.ultimate_strength = concrete_dff['stress'].max()
+            concrete_dff['rolling'] = concrete_dff['stress'].rolling(50).mean()
+            reg_line = linregress(concrete_dff['strain'].head(5000), concrete_dff['stress'].head(5000))
+            self.youngs_modulus = reg_line.slope
+            print(self.specimen_name + " Ultimate Strength: %f. Young's Modulus: %f" % (
+            self.ultimate_strength, self.youngs_modulus))
+            self.data_table = concrete_dff
+
+            plt.figure(dpi=300)
+            plt.plot(concrete_dff['strain'], concrete_dff['stress'],
+                     label='Structural Analysis of %s' % self.specimen_name)
+            plt.plot(concrete_dff['strain'], reg_line.intercept + self.youngs_modulus * concrete_dff['strain'],
+                     label='Regression Line = %fx + %f with R^2 %f' % (
+                     self.youngs_modulus, reg_line.intercept, reg_line.rvalue))
+            plt.plot(concrete_dff['strain'], concrete_dff['rolling'], label='Rolling Average')
+            plt.title(
+                'Stress vs. Strain of %s with Ultimate Strength %f' % (self.specimen_name, self.ultimate_strength))
+            plt.xlabel('Strain (in/in)')
+            plt.ylabel('Stress (psi)')
+            plt.ylim(0, self.ultimate_strength)
+            plt.legend()
+            plt.savefig('%s plot.png' % self.specimen_name, dpi=500)
 
         #draw_figure(_VARS['window']['figCanvas'].TKCanvas, fig)
 
