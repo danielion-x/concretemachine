9b44c1bdbd3fa1a57f7a62c08a466fe0942676d2
Alexander Khong <aokhong@dons.usfca.edu>
2023-04-30 23:36:52 -0700

    Deleted the Files at the bottom

--- concrete_analysis.py
+++ concrete_analysis.py
@@ -29,42 +29,24 @@ class concrete_specimen:
         self.units = units
 
     def unit_conversion_metric2imperial(self):
-        self.fine_aggregate = fineaggpcnt * 0.00220462
-        'grams to lbs'
-        self.coarse_aggregate_percent = coarseaggpcnt * 0.00220462
-        'grams to lbs'
-        self.water_percent = waterpcnt * 0.00220462
-        'grams to lbs'
-        self.cement_percent = cementpcnt * 0.00220462
-        'grams to lbs'
-        self.blast_furnace_slag = blast_fer_slg
-        0.00220462
-        'grams to lbs'
-        self.fly_ash = flyash * 0.00220462
-        'grams to lbs'
-        self.super_plastisizer = superplast * 0.00220462
-        'grams to lbs'
-        self.radius = radius * 39.3701
-        'meters to inches'
+        self.fine_aggregate = fineaggpcnt * 0.00220462 'grams to lbs'
+        self.coarse_aggregate_percent = coarseaggpcnt * 0.00220462'grams to lbs'
+        self.water_percent = waterpcnt * 0.00220462'grams to lbs'
+        self.cement_percent = cementpcnt * 0.00220462'grams to lbs'
+        self.blast_furnace_slag = blast_fer_slg * 0.00220462'grams to lbs'
+        self.fly_ash = flyash * 0.00220462 'grams to lbs'
+        self.super_plastisizer = superplast * 0.00220462 'grams to lbs'
+        self.radius = radius * 39.3701 'meters to inches'
 
     def unit_conversion_imperial2metric(self):
-        self.fine_aggregate = fineaggpcnt * 453.592
-        'lbs to grams'
-        self.coarse_aggregate_percent = coarseaggpcnt * 453.592
-        'lbs to grams'
-        self.water_percent = waterpcnt * 453.592
-        'lbs to grams'
-        self.cement_percent = cementpcnt * 453.592
-        'lbs to grams'
-        self.blast_furnace_slag = blast_fer_slg
-        453.592
-        'lbs to grams'
-        self.fly_ash = flyash * 453.592
-        'lbs to grams'
-        self.super_plastisizer = superplast * 453.592
-        'lbs to grams'
-        self.radius = radius * 0.0254
-        'inches 2 meter'
+        self.fine_aggregate = fineaggpcnt * 453.592 'lbs to grams'
+        self.coarse_aggregate_percent = coarseaggpcnt * 453.592'lbs to grams'
+        self.water_percent = waterpcnt * 453.592 'lbs to grams'
+        self.cement_percent = cementpcnt * 453.592 'lbs to grams'
+        self.blast_furnace_slag = blast_fer_slg * 453.592 'lbs to grams'
+        self.fly_ash = flyash * 453.592 'lbs to grams'
+        self.super_plastisizer = superplast * 453.592 'lbs to grams'
+        self.radius = radius * 0.0254 'inches 2 meter'
 
     def concreteAnalysis(self):
         ''' Takes an Excel file of inches vs kips and produces a graphical representation including
